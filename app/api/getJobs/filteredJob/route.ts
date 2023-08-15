3aad87d83da535adc47028d1738ca5e503806bad
AbdelilahBKM <boukhatemabdelilah420@gmail.com>
2023-08-15 16:51:15 +0100

    deployement
    prisma deployment fix

--- app/api/getJobs/filteredJob/route.ts
+++
@@ -1,8 +0,0 @@
-import { NextRequest, NextResponse } from "next/server";
-const {PrismaClient} = require("@prisma/client")
-
-const prisma = new PrismaClient()
-
-export async function GET(request : NextRequest) {
-    
-}
