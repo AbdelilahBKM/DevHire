c5eceb6a4dab11d349a526765e72978a7c810b97
AbdelilahBKM <boukhatemabdelilah420@gmail.com>
2023-08-16 13:25:17 +0100

    migrate database
    Migrating database to mySQL db hosted by Railway.com

--- prisma/migrations/20230816121246_inithost/migration.sql
+++ prisma/migrations/20230816121246_inithost/migration.sql
@@ -5,6 +5,7 @@ CREATE TABLE `User` (
     `email` VARCHAR(255) NOT NULL,
     `password` VARCHAR(255) NOT NULL,
     `fullname` VARCHAR(255) NOT NULL,
+    `phone_number` VARCHAR(64) NOT NULL,
     `avatar` VARCHAR(191) NULL,
     `resume` VARCHAR(191) NOT NULL,
     `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
@@ -21,8 +22,6 @@ CREATE TABLE `Technology` (
     `name` VARCHAR(191) NOT NULL,
     `value` VARCHAR(191) NOT NULL,
 
-    UNIQUE INDEX `Technology_name_key`(`name`),
-    UNIQUE INDEX `Technology_value_key`(`value`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
@@ -42,23 +41,15 @@ CREATE TABLE `Job` (
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- CreateTable
-CREATE TABLE `Applied` (
-    `user_id` VARCHAR(191) NOT NULL,
-    `job_id` VARCHAR(191) NOT NULL,
-    `application_date` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
-
-    UNIQUE INDEX `Applied_user_id_key`(`user_id`),
-    UNIQUE INDEX `Applied_job_id_key`(`job_id`),
-    PRIMARY KEY (`user_id`, `job_id`)
-) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-
--- CreateTable
 CREATE TABLE `Company` (
     `id` VARCHAR(191) NOT NULL,
     `name` VARCHAR(191) NOT NULL,
     `logo` VARCHAR(191) NULL,
     `location` VARCHAR(191) NOT NULL,
+    `adress` VARCHAR(191) NULL,
+    `city` VARCHAR(191) NULL,
     `website` VARCHAR(191) NULL,
+    `phone_number` VARCHAR(64) NOT NULL,
     `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
     `updatedAt` DATETIME(3) NOT NULL,
 
@@ -83,14 +74,17 @@ CREATE TABLE `_savedJobs` (
     INDEX `_savedJobs_B_index`(`B`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
+-- CreateTable
+CREATE TABLE `_appliedTo` (
+    `A` VARCHAR(191) NOT NULL,
+    `B` VARCHAR(191) NOT NULL,
+
+    UNIQUE INDEX `_appliedTo_AB_unique`(`A`, `B`),
+    INDEX `_appliedTo_B_index`(`B`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
 -- AddForeignKey
 ALTER TABLE `Job` ADD CONSTRAINT `Job_companyId_fkey` FOREIGN KEY (`companyId`) REFERENCES `Company`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE `Applied` ADD CONSTRAINT `Applied_user_id_fkey` FOREIGN KEY (`user_id`) REFERENCES `User`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE `Applied` ADD CONSTRAINT `Applied_job_id_fkey` FOREIGN KEY (`job_id`) REFERENCES `Job`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE `_jobTechnologies` ADD CONSTRAINT `_jobTechnologies_A_fkey` FOREIGN KEY (`A`) REFERENCES `Job`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
@@ -103,3 +97,9 @@ ALTER TABLE `_savedJobs` ADD CONSTRAINT `_savedJobs_A_fkey` FOREIGN KEY (`A`) RE
 
 -- AddForeignKey
 ALTER TABLE `_savedJobs` ADD CONSTRAINT `_savedJobs_B_fkey` FOREIGN KEY (`B`) REFERENCES `User`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `_appliedTo` ADD CONSTRAINT `_appliedTo_A_fkey` FOREIGN KEY (`A`) REFERENCES `Job`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `_appliedTo` ADD CONSTRAINT `_appliedTo_B_fkey` FOREIGN KEY (`B`) REFERENCES `User`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
