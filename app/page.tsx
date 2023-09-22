5fb0349705f0788aa913317fafb054ac24c916d8
HOPE <boukhatemabdelilah420@gmail.com>
2023-09-22 16:01:33 +0200

    no database push

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,18 @@
-import JobListing from '@/components/ui/jobListing'
+import JobListing, { jobsProps } from '@/components/ui/jobListing'
+import { Job } from './types'
 
 
-const getJobs = async () => {
-  const API_URL = process.env.API_URL
-  const res = await fetch(`${API_URL}/api/getJobs`)
-  return res.json()
-}
+// const getJobs = async () => {
+//   const API_URL = process.env.API_URL
+//   const res = await fetch(`${API_URL}/api/getJobs`)
+//   return res.json()
+// }
 
 export default async function Home(){
   
-  const data = await getJobs()
-  const {jobs} = data
+  // const data = await getJobs()
+  // const {jobs} = data
+  const jobs :Job[] = []
 
   return (
     <main className="relative text-gray-700 flex justify-center items-center lg:mt-[150px]">
