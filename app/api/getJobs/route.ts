bb33702e3f03e2d93db9fd58737187d16c182293
HOPE <boukhatemabdelilah420@gmail.com>
2023-09-22 16:05:34 +0200

    Update route.ts

--- app/api/getJobs/route.ts
+++ app/api/getJobs/route.ts
@@ -1,24 +1,24 @@
-import { NextResponse } from "next/server"
-import {PrismaClient} from "@prisma/client"
+// import { NextResponse } from "next/server"
+// import {PrismaClient} from "@prisma/client"
 
-const prisma = new PrismaClient()
+// const prisma = new PrismaClient()
 
-export async function GET(){
-    try{
+// export async function GET(){
+//     try{
 
-        const jobs = await prisma.job.findMany({
-            include: {
-                technologies: true,
-                company: true
-            },
-            take: 10
-        })
-        return NextResponse.json({jobs})
+//         const jobs = await prisma.job.findMany({
+//             include: {
+//                 technologies: true,
+//                 company: true
+//             },
+//             take: 10
+//         })
+//         return NextResponse.json({jobs})
 
-    } catch( err ){
-        console.log(err)
-        return NextResponse.error()
-    } finally{
-        await prisma.$disconnect()
-    }
-}
+//     } catch( err ){
+//         console.log(err)
+//         return NextResponse.error()
+//     } finally{
+//         await prisma.$disconnect()
+//     }
+// }
